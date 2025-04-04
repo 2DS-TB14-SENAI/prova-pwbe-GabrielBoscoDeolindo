d5f78f0fe5580452878e1d227935c1f5e4778515
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-04 16:42:46 -0300

    is_verified

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -6,6 +6,7 @@ class CustomUser(AbstractUser):
     phone = models.CharField(unique=True, max_length=35)
     address = models.TextField(blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
