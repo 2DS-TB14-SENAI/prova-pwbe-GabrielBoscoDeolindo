d6ae3a5bad350afb5e1fffba3d3154172abe1f48
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-04 14:59:55 -0300

    esqueci as urls do projeto

--- servicos/urls.py
+++ servicos/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('clinica.urls'))
+    path('',include('clinica.urls')),
+    path('',include('agenda.urls'))
 ]
