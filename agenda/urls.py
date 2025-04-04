7ab92ce1363c7c8b8e280f7577d6d21c9514cbbe
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-04 15:04:26 -0300

    agendamentos feitosgit add .

--- agenda/urls.py
+++ agenda/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('api/servicos/', views.criar_servico),
     path('api/servicos/', views.listar_servicos),
     path('api/servicos/<int:pk>', views.servico_especifico),
+    path('api/agendamentos/', views.criar_agendamento),
+    path('api/agendamentos/', views.listar_agendamento),
+    path('api/agendamentos/<int:pk>', views.agendamento_especifico),
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -29,3 +29,28 @@ def criar_servico(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def listar_agendamento(request):
+    agendamento = Agendamento.objects.all()
+    serializer = AgendamentoSerializer(agendamento, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def agendamento_especifico(request, pk):
+    try:
+        agendamentos = Agendamento.objects.get(pk=pk)
+    except agendamentos.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = AgendamentoSerializer(agendamentos)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def criar_agendamento(request):
+    serializer = AgendamentoSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
