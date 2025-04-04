ebd9dd0c866251a648eeb97985cf79f32e99d835
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-04 14:23:53 -0300

    GET E POST FUNCIONANDO

--- clinica/urls.py
+++ clinica/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/medicos/', views.listar_medicos),
-    path('/consultas/nova/', views.criar_consulta),
+    path('medicos/', views.listar_medicos),
+    path('consultas/nova/', views.criar_consulta),
     # path('/consultas/<int:id>/', views.detalhes_consulta),
 ]
